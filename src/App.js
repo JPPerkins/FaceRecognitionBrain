f807f063763b6a1c64b7806195edf1fc2026732b
JPPerkins <jcperkins482@gmail.com>
2024-01-30 18:09:03 -0500

    Added first pass at FaceRecognition API integration.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import Clarifai from 'clarifai';
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg color="#FFFFFF" num={175} type="cobweb" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-        { /*<FaceRecognition /> */}
-     </div>
-  );
+const app = new Clarifai.App({
+  apiKey: '97ef50b5bd4e402d92bfc1b218fd6806'
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageURL: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err)
+      {
+        //there are an error
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg color="#FFFFFF" num={175} type="cobweb" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageUrl={this.state.imageURL}/>
+       </div>
+    );
+  }
 }
 
 export default App;
