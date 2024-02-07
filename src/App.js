30ce6e7a357fe870a8809ae770b8b275bb447b41
JPPerkins <jcperkins482@gmail.com>
2024-02-06 19:34:42 -0500

    Changed wording in Rank.js and App.js, added functionality to onPictureSubmit to increase entrycount.

--- src/App.js
+++ src/App.js
@@ -64,13 +64,28 @@ class App extends Component {
     this.setState({input: event.target.value});
   }
 
-  onButtonSubmit = () => {
+  onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input});
     app.models
       .predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+          if (response) {
+            fetch('http://localhost:3000/image', {
+              method: 'put',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                id: this.state.user.id
+              })
+            })
+              .then(response => response.json())
+              .then(count => {
+                this.setState(Object.assign(this.state.user, {entries: count}));
+              })
+          }
+          this.displayFaceBox(this.calculateFaceLocation(response)) 
+        })
       .catch(err => console.log(err));
   }
 
@@ -95,7 +110,7 @@ class App extends Component {
           <Rank name={this.state.user.name} entries={this.state.user.entries} />
           <ImageLinkForm 
             onInputChange={this.onInputChange} 
-            onButtonSubmit={this.onButtonSubmit}
+            onButtonSubmit={this.onPictureSubmit}
           />
           <FaceRecognition box={box} imageUrl={imageUrl}/>
         </div>
